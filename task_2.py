38601251e9536cba5fa7f46e4315ac9929704223
Bohdan-Chekhirkin <ist22-b.chekhirkin@nubip.edu.ua>
2024-11-27 21:21:01 +0200

    Виправлення Завдання 2

--- task_2.py
+++ task_2.py
@@ -4,13 +4,12 @@ def get_numbers_ticket(min, max, quantity):
 	min = int(min)
 	max = int(max)
 	quantity = int(quantity)
-	if min >= 1 and max <= 1000:
+	if (min >= 1 and max <= 1000) and (max > 0 and min > 0) and min < max and (max - min > quantity):
 		ticket = random.sample(range(min, max+1), quantity)
 		sorted_ticket = ticket.sort()
 	else:
-		print("Введіть числа від 1 до 1000")
-		return None
+		ticket = []
 
-	return print(ticket)
+	return ticket
 
-get_numbers_ticket(1,49,6)
+print(get_numbers_ticket(-10,14,6))
