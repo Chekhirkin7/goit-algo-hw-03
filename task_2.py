61ee8a5ac944997cefed939acebd26b5d1d83a6c
Bohdan-Chekhirkin <ist22-b.chekhirkin@nubip.edu.ua>
2024-11-27 21:28:00 +0200

    Завдання 2 повне виправлення

--- task_2.py
+++ task_2.py
@@ -1,15 +1,15 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-	min = int(min)
-	max = int(max)
-	quantity = int(quantity)
-	if (min >= 1 and max <= 1000) and (max > 0 and min > 0) and min < max and (max - min > quantity):
-		ticket = random.sample(range(min, max+1), quantity)
-		sorted_ticket = ticket.sort()
-	else:
-		ticket = []
+    
+    if not (1 <= min <= max <= 1000):
+        return []
+    if not (1 <= quantity <= (max - min + 1)):
+        return []
 
-	return ticket
+    numbers = random.sample(range(min, max + 1), quantity)
 
-print(get_numbers_ticket(-10,14,6))
+    return sorted(numbers)
+
+lottery_numbers = get_numbers_ticket(10,14,3)
+print("Ваші лотерейні числа:", lottery_numbers)
